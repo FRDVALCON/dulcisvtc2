f9156d877409e2db545c05006c4a53ef32bf9e6d
Alex <me@djoh.xyz>
2022-08-05 14:05:07 +0000

    fix position formatting

--- content/assets/js/leaderboard.js
+++ content/assets/js/leaderboard.js
@@ -4,26 +4,25 @@ fetch("https://api.dulcisvtc.com/users").then((res) => res.json()).then((data) =
         .sort((a, b) => b.leaderboard.monthly_mileage - a.leaderboard.monthly_mileage)
         .filter((member) => member.leaderboard.monthly_mileage > 1);
 
-    // create a div element with class "luser" for each of the data items
     const users = data.map((user, i) => `
         <div class="luser">
             <p><strong>${formatPosition(i + 1)}</strong></p>
-            <p>${user.username}</p>
+            <p>${user.username.match(/[-[\]{}()*+?.,\\^$|#\s\da-z]/gi).join("").trim()}</p>
             <p>${Math.round(user.leaderboard.monthly_mileage)} km</p>
         </div>
     `).join("");
 
     leaderboardDiv.innerHTML = users;
-
-    function formatPosition(i) {
-        if (i === 1) {
-            return "1st";
-        } else if (i === 2) {
-            return "2nd";
-        } else if (i === 3) {
-            return "3rd";
-        } else {
-            return `${i}th`;
-        };
-    };
 });
+
+function formatPosition(i) {
+    if (`${i}`.endsWith("1")) {
+        return `${i}st`;
+    } else if (`${i}`.endsWith("2")) {
+        return `${i}nd`;
+    } else if (`${i}`.endsWith("3")) {
+        return `${i}rd`;
+    } else {
+        return `${i}th`;
+    };
+};

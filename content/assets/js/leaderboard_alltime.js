3fa9323d10672a8a3bcddcdd0fb94b25d86793a2
Alex <me@djoh.xyz>
2022-08-27 13:38:43 +0000

    split leaderabords

---
+++ content/assets/js/leaderboard_alltime.js
@@ -0,0 +1,28 @@
+fetch("https://api.dulcisvtc.com/users").then((res) => res.json()).then((data) => {
+    const leaderboardDiv = document.getElementById("leaderboard");
+    data = data
+        .sort((a, b) => b.leaderboard.alltime_mileage - a.leaderboard.alltime_mileage)
+        .filter((member) => member.leaderboard.alltime_mileage > 1);
+
+    const users = data.map((user, i) => `
+        <div class="luser">
+            <p><strong>${formatPosition(i + 1)}</strong></p>
+            <p>${user.username.match(/[-[\]{}()*+?.,\\^$|#\s\da-z]/gi).join("").trim()}</p>
+            <p>${Math.round(user.leaderboard.alltime_mileage)} km</p>
+        </div>
+    `).join("");
+
+    leaderboardDiv.innerHTML = users;
+});
+
+function formatPosition(i) {
+    if (`${i}`.endsWith("1") && !`${i}`.endsWith("11")) {
+        return `${i}st`;
+    } else if (`${i}`.endsWith("2") && !`${i}`.endsWith("12")) {
+        return `${i}nd`;
+    } else if (`${i}`.endsWith("3") && !`${i}`.endsWith("13")) {
+        return `${i}rd`;
+    } else {
+        return `${i}th`;
+    };
+};

9d9ef3fd57696e62fdd30063cec02cfd3945d0ce
Alex <me@djoh.xyz>
2022-09-10 10:35:28 +0000

    fixes
    * Fixed showing distance instead of used fuel in fuel cell
    * Fixed blank city & company names

--- content/assets/js/dhub.js
+++ content/assets/js/dhub.js
@@ -1,27 +1,25 @@
-fetch("https://api.dulcisvtc.com/jobs").then((res) => {
-    res.json().then((data) => {
-        const jobsTable = document.getElementById("jobdata").getElementsByTagName('tbody')[0];
+fetch("https://api.dulcisvtc.com/jobs").then((res) => res.json()).then((data) => {
+    const jobsTable = document.getElementById("jobdata").getElementsByTagName('tbody')[0];
 
-        data.reverse().map((itemData) => {
-            const row = jobsTable.insertRow();
+    data.reverse().map((itemData) => {
+        const row = jobsTable.insertRow();
 
-            const username = row.insertCell();
-            const sourceCity = row.insertCell();
-            const sourceCompany = row.insertCell();
-            const destinationCity = row.insertCell();
-            const destinationCompany = row.insertCell();
-            const cargo = row.insertCell();
-            const drivenDistance = row.insertCell();
-            const fuelUsed = row.insertCell();
+        const username = row.insertCell();
+        const sourceCity = row.insertCell();
+        const sourceCompany = row.insertCell();
+        const destinationCity = row.insertCell();
+        const destinationCompany = row.insertCell();
+        const cargo = row.insertCell();
+        const drivenDistance = row.insertCell();
+        const fuelUsed = row.insertCell();
 
-            username.innerHTML = itemData.driver.username;
-            sourceCity.innerHTML = itemData.source_city;
-            sourceCompany.innerHTML = itemData.source_company;
-            destinationCity.innerHTML = itemData.destination_city;
-            destinationCompany.innerHTML = itemData.destination_company;
-            cargo.innerHTML = itemData.cargo.name;
-            drivenDistance.innerHTML = `${Math.round(itemData.driven_distance).toLocaleString()}km`;
-            fuelUsed.innerHTML = `${Math.round(itemData.driven_distance).toLocaleString()}L`;
-        });
+        username.innerHTML = itemData.driver.username;
+        sourceCity.innerHTML = itemData.source_city ?? "[unknown]";
+        sourceCompany.innerHTML = itemData.source_company ?? "[unknown]";
+        destinationCity.innerHTML = itemData.destination_city ?? "[unknown]";
+        destinationCompany.innerHTML = itemData.destination_company ?? "[unknown]";
+        cargo.innerHTML = itemData.cargo.name;
+        drivenDistance.innerHTML = `${Math.round(itemData.driven_distance).toLocaleString()}km`;
+        fuelUsed.innerHTML = `${Math.round(itemData.fuel_used).toLocaleString()}L`;
     });
 });
